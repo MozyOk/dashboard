a8f2cc1675ed99802bc807d1c6475043b3e6c4c9
Masahito Okubo 'Mozy <mozy.okubo@gmail.com>
2018-08-03 06:30:06 +0300

    fix: size

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ const uppy = Uppy.Core({ autoProceed: false });
 uppy.use(Uppy.Dashboard, {
   target: "#drag-drop-area",
   inline: true,
-  height: 450
+  height: 400,
+  width: 400
 });
 uppy.use(Uppy.Webcam, { target: Uppy.Dashboard });
 uppy.use(Uppy.Tus, { endpoint: "https://master.tus.io/files/" });
